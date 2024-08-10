5e71737aa1be54a547ffbc9f6134ca9c2e91a25d
NayefNemernn <nayefnemernn@gmail.com>
2024-08-10 15:59:36 +0300

    Deleting Cards dir, changing notes functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Card from './components/Cards/Card'
-import contacts from './components/Cards/contacts'
+import Note from './components/Note'
+import notes from './notes'
+
 
 function App() {
-  const contact1 = contacts[0]; // First contact
-  const contact2 = contacts[1]; // Second contact
+
   return (
     <>
-      <div className="cards-container">
-        <Card
-          name={contact1.name}
-          imgURL={contact1.imgURL}
-          phone={contact1.phone}
-          email={contact1.email}
-        />
-        <Card
-          name={contact2.name}
-          imgURL={contact2.imgURL}
-          phone={contact2.phone}
-          email={contact2.email}
-        />
-        <Card
-          name={contacts[2].name}
-          imgURL={contacts[2].imgURL}
-          phone={contacts[2].phone}
-          email={contacts[2].email}
-        />
-      </div>
+    <Header />
+  
+      {notes.map((note) => <Note key = {note.key} title = {note.title} content = {note.content}/>)}
+
+      <Footer />
     </>
   )
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Note(){
-    return <>
-    <h1></h1>
+function Note(props){
+    return <> 
+    <div className="note">
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
+    </div>
     </>
 }
+export default Note;
